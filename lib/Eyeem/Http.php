501bfb202829d76ab780a00e1463c27dbe6de447
François Hodierne <francois@hodierne.net>
2012-02-28 12:23:13 +0100

    handle POST parameters

--- lib/Eyeem/Http.php
+++ lib/Eyeem/Http.php
@@ -12,22 +12,39 @@ class Eyeem_Http
     return self::request($url, 'GET', $params);
   }
 
+  public static function post($url, $params = array())
+  {
+    return self::request($url, 'POST', $params);
+  }
+
   public static function request($url, $method = 'GET', $params = array())
   {
-      if (!empty($params)) {
-        $url .= (strpos($url, '?') === false ? '?': '&') . http_build_query($params);
-      }
+      // echo "$method:$url\n";
       if (function_exists('curl_init')) {
           $ch = curl_init();
-          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+          // Method
+          if ($method == 'POST') {
+            curl_setopt($ch, CURLOPT_POST, true);
+          } else {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+          }
+          // Parameters
+          if (!empty($params)) {
+            if ($method == 'GET') {
+              $url .= (strpos($url, '?') === false ? '?': '&') . http_build_query($params, null, '&');
+            } else {
+              curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+            }
+          }
+          curl_setopt($ch, CURLOPT_URL, $url);
           curl_setopt($ch, CURLOPT_HEADER, false);
           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-          curl_setopt($ch, CURLOPT_URL, $url);
           curl_setopt($ch, CURLOPT_USERAGENT, self::$userAgent . ' (curl)');
           curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, self::$timeout);
           $content = curl_exec($ch);
           curl_close($ch);
       } else {
+          throw new Exception('Curl not available.');
           $httpParams = array(
             'method' => $method,
             'user_agent' => self::$userAgent . '(php)',
