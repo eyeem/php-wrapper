10622f16aa73fe4ee2e19d204e06382d838ce6ef
François Hodierne <francois@hodierne.net>
2012-03-29 14:13:55 +0200

    fix comment fetching

--- lib/Eyeem/Ressource/Comment.php
+++ lib/Eyeem/Ressource/Comment.php
@@ -3,9 +3,9 @@
 class Eyeem_Ressource_Comment extends Eyeem_Ressource
 {
 
-  public static $name = 'album';
+  public static $name = 'comment';
 
-  public static $endpoint = '/photos/{photoId}/comments/{id}';
+  public static $endpoint = '/comments/{id}';
 
   public static $properties = array(
     'id',
@@ -16,17 +16,6 @@ class Eyeem_Ressource_Comment extends Eyeem_Ressource
   );
 
   public static $collections = array();
-
-  public function getEndpoint()
-  {
-    if (empty($this->id)) {
-      throw new Exception("Unknown id.");
-    }
-    $endpoint = static::$endpoint;
-    $endpoint = str_replace('{photoId}', $this->getPhotoId(), $endpoint);
-    $endpoint = str_replace('{id}', $this->id, $endpoint);
-    return $endpoint;
-  }
 
   public function getUser()
   {
